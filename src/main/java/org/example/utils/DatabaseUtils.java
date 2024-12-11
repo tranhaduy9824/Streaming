51834ec5dd02c32aa7288662f2c41b92edbfe8ff
ducdoan-04 <hoducdoan2004@gmail.com>
2024-12-11 21:29:54 +0700

    update login

--- src/main/java/org/example/utils/DatabaseUtils.java
+++ src/main/java/org/example/utils/DatabaseUtils.java
@@ -8,7 +8,7 @@ public class DatabaseUtils {
     private static final String URL = "jdbc:mysql://localhost:3306/streaming";
     // private static final String URL = "jdbc:mysql://192.168.202.52:3306/streaming";
     private static final String USER = "root";
-    private static final String PASSWORD = "12345";
+    private static final String PASSWORD = "";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
